b2b9f960c22c176955b63394a3bb3731dc103e3c
Kjetil <Kjetil_T@hotmail.com>
2024-08-23 10:25:57 +0200

    dto-update

--- SKSBookingAPI/Models/Booking.cs
+++ SKSBookingAPI/Models/Booking.cs
@@ -10,8 +10,16 @@
     public class BookingDTO
     {
         public int BookingID { get; set; }
-        public Rental Rental { get; set; }
         public DateTime BookedFrom { get; set; }
         public DateTime BookedUntil { get; set; }
+    }
+
+    public class CreateBookingDTO
+    {
+        public required int BookingID { get; set; }
+        public required User UserRenting { get; set; }
+        public required Rental Rental { get; set; }
+        public required DateTime BookedFrom { get; set; }
+        public required DateTime BookedUntil { get; set; }
     }
 }
